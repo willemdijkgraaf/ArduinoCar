28cc793463896e37e7540ec67f2361fd9d8c7f7a
Wim Dijkgraaf <wim.dijkgraaf@quotationfactory.com>
2025-08-30 19:01:25 -0300

    Move speed to a variable.

--- MotorDriverAndTwoMotors/MotorDriverAndTwoMotors.ino
+++ MotorDriverAndTwoMotors/MotorDriverAndTwoMotors.ino
@@ -7,6 +7,8 @@ const uint8_t ENB = 5;  // Left motor enable (PWM)
 const uint8_t IN3 = 7;   // Left motor dir A
 const uint8_t IN4 = 6;   // Left motor dir B
 
+uint8_t speed = 255;
+
 void setup() {
   // Set all pins as outputs
   pinMode(ENA, OUTPUT);
@@ -15,15 +17,12 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-
-  
-  
 }
 
 void loop() {
   // Nothing needed here, keeps running forward
-  analogWrite(ENA, 255); // full speed left motor
-  analogWrite(ENB, 255); // full speed right motor
+  analogWrite(ENA, speed); // full speed left motor
+  analogWrite(ENB, speed); // full speed right motor
 
   // Drive both motors forward at full speed
   digitalWrite(IN1, LOW);
